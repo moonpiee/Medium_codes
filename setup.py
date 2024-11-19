16f5634965c71e169106bd1ee0d27daad049e54b
ChanPie <96528075+moonpiee@users.noreply.github.com>
2024-11-19 17:24:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import google.generativeai as genai
 from kaggle_secrets import UserSecretsClient
+#Configure google api key 
 GOOGLE_API_KEY=UserSecretsClient().get_secret("GOOGLE_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
